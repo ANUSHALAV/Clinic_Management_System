016e6523d47fbafd5413f1ad4f417a464f88f988
ANUSHALAV <bhattanushalav@gmail.com>
2026-04-23 20:20:22 +0530

    add controller for get doctors and get doctor by clinic id and doctor id and add model for doctor response and add doctor

--- Clinic_Management_System/Controllers/DoctorController.cs
+++ Clinic_Management_System/Controllers/DoctorController.cs
@@ -1,14 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clinic_Management_System.Models.Responses;
+using Clinic_Management_System.Services.Implementations;
+using Clinic_Management_System.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Clinic_Management_System.Controllers
 {
     [Route("api/[controller]")]
     public class DoctorController : ControllerBase
     {
-        public DoctorController()
+        private readonly IDoctorService _doctorService;
+        public DoctorController(IDoctorService doctorService)
         {
-        } 
-        
+            _doctorService = doctorService;
+        }
+
+        [HttpGet]
+        [Route("DoctorsByClinic")]
+        public async Task<IActionResult> GetDoctrorsByClinicId(string ClinicId)
+        {
+            var res = new APIResponse();
+            try
+            {
+                var Data = await _doctorService.GetDoctorsByClinicId(ClinicId);
+                if (Data != null)
+                {
+                    res.Data = Data;
+                    res.Success = true;
+                    res.Message = "Doctors retrieved successfully.";
+                    res.TotalRecorde = Data.Count;
+                }
+                else
+                {
+                    res.Success = false;
+                    res.Message = "No doctors found for the specified clinic.";
+                    return NotFound(res);
+                }
+            }
+            catch (Exception ex)
+            {
+                res.Success = false;
+                res.Message = ex.Message;
+                return BadRequest(res);
+            }
+            return Ok(res);
+        }
+
+        [HttpGet]
+        [Route("DoctorByDoctorIdAndClinicId")]
+        public async Task<IActionResult> GetDoctrorsByDoctorIdAndClinicId(string UserId, string ClinicId)
+        {
+            var res = new APIResponse();
+            try
+            {
+                var Data = await _doctorService.GetDoctorsByDoctorIdAndClinicId(UserId, ClinicId);
+                if (Data != null)
+                {
+                    res.Data = Data;
+                    res.Success = true;
+                    res.Message = "Doctor retrieved successfully.";
+                }
+                else
+                {
+                    res.Success = false;
+                    res.Message = "No doctor found for the specified clinic.";
+                    return NotFound(res);
+                }
+            }
+            catch (Exception ex)
+            {
+                res.Success = false;
+                res.Message = ex.Message;
+                return BadRequest(res);
+            }
+            return Ok(res);
+        }
+
 
     }
 }
--- Clinic_Management_System/Services/Interfaces/IDoctorService.cs
+++ Clinic_Management_System/Services/Interfaces/IDoctorService.cs
@@ -1,6 +1,12 @@
-﻿namespace Clinic_Management_System.Services.Interfaces
+﻿using Clinic_Management_System.Models.DTOs;
+using Clinic_Management_System.Models.Responses;
+
+namespace Clinic_Management_System.Services.Interfaces
 {
     public interface IDoctorService
     {
+        public Task<List<DoctorResponse>> GetDoctorsByClinicId(string ClinicId);
+        public Task<DoctorResponse> GetDoctorsByDoctorIdAndClinicId(string UserId, string ClinicId);
+        public Task<AddDoctorDTO> AddDoctorsByClinicId(AddDoctorDTO Obj);
     }
 }

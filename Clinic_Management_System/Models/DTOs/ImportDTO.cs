4d2c2dfe8a1e6a4221b94f9aaff6f8a2ad4122c3
ANUSHALAV <bhattanushalav@gmail.com>
2026-07-11 21:58:17 +0530

    Add ImportDTO and SearchDTO for import data handling
    Introduced ImportDTO.cs with ImportDTO and SearchDTO classes to support data transfer for import operations. Includes properties for clinic, user, department IDs, search criteria, pagination, date range with BSON options, and status.

---
+++ Clinic_Management_System/Models/DTOs/ImportDTO.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Runtime.CompilerServices;
+
+namespace Clinic_Management_System.Models.DTOs
+{
+    public class ImportDTO
+    {
+        public string ClinicId { get; set; }
+        public string UserId { get; set; }
+        public string UserTypeId { get; set; }
+        public string DepartmentId { get; set; }
+        public List<SearchDTO> SearchList { get; set; }
+        public int PageNumber { get; set; }
+        public int DataLimit { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime ToDate { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime FromDate { get; set; }
+        public int Status { get; set; }
+    }
+
+    public class SearchDTO
+    {
+        public string SearchBy { get; set; }
+        public string SearchValue { get; set; }
+        public string[] SearchArray { get; set; }
+    }
+}
